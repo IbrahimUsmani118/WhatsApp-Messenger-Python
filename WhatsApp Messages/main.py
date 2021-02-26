cb01e1e8075fa2d977d88914be5307b2c0225d92
Ibrahim Usmani <78166163+Iusmani123@users.noreply.github.com>
2021-02-26 14:34:04 -0500

    Add files via upload

---
+++ WhatsApp Messages/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 11:24:32 2021
+
+@author: onlyi
+"""
+# Import library to start the module
+import pywhatkit
+
+# Enter a phone number
+number = input("Enter a phone number with the country's extension: ")
+
+# Enter a message
+msg = input("Enter a message you would like to send: ")
+
+# Enter the hour
+hour = float(input("Enter the hour number you would like it to be sent(0-23): "))
+
+# Enter the minute
+minute = float(input("Enter the minute you would like it to be sent(0-59): "))
+
+# The function which calls the previously defined variables
+pywhatkit.sendwhatmsg(number, msg, hour, minute)
+
+input()
---
+++ WhatsApp Messages/readme.txt
@@ -0,0 +1,12 @@
+Created by Ibrahim Usmani
+
+What this project is:
+This project is basically a python program which prompts user to enter a specific time(number of hour and minute) 
+and phone number with country code along with the message they would like to send while being logged in the
+WhatsApp application. 
+
+Modules used:
+pywhatkit
+
+How to install:
+enter this in the console: pip install pywhatkit
